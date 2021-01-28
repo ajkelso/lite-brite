fbb73f91a1876c176aab36cd8d43785bc1e810e8
ajkelso <ajkelso@gmail.com>
2021-01-27 21:25:57 -0500

    add eventListeners to color buttons

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -7,4 +7,13 @@ const greenSelector = document.querySelector('#green-button')
 const purpleSelector = document.querySelector('#purple-button')
 
 let currentColor = null
+const selectors = [redSelector, yellowSelector, blueSelector, greenSelector, purpleSelector]
+
+selectors.forEach(selector => {
+    selector.addEventListener('click', function(e) {
+        currentColor = selector.innerText
+        console.log(currentColor)
+    })
+})
+
 
