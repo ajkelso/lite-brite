67086de765bb71d28752be7c4e30cb8bcb2789bd
ajkelso <ajkelso@gmail.com>
2021-01-31 15:47:35 -0500

    refactor row creation

--- frontend/scripts/components/canvas.js
+++ frontend/scripts/components/canvas.js
@@ -30,16 +30,21 @@ class Canvas {
     createRows = () => {
         
         //refactor this business!
+        let rows = []
+        for (let i = 0; i < this.pegs.length / 10; i++) {
+            let row = this.pegs.slice((10 * i), ((10 * i) + 10))
+            rows.push(row)
+        }
         
-        let row1 = this.pegs.slice(0, 10)
-        let row2 = this.pegs.slice(10, 20)
-        let row3 = this.pegs.slice(20, 30)
-        let row4 = this.pegs.slice(30, 40)
-        let row5 = this.pegs.slice(40, 50)
-        let row6 = this.pegs.slice(50, 60)
-        let row7 = this.pegs.slice(60, 70)
-        let row8 = this.pegs.slice(70)
-        let rows = [row1, row2, row3, row4, row5, row6, row7, row8]
+        // let row1 = this.pegs.slice(0, 10)
+        // let row2 = this.pegs.slice(10, 20)
+        // let row3 = this.pegs.slice(20, 30)
+        // let row4 = this.pegs.slice(30, 40)
+        // let row5 = this.pegs.slice(40, 50)
+        // let row6 = this.pegs.slice(50, 60)
+        // let row7 = this.pegs.slice(60, 70)
+        // let row8 = this.pegs.slice(70)
+        // let rows = [row1, row2, row3, row4, row5, row6, row7, row8]
         for (let i = 0; i < rows.length; i++) {
             let div = document.createElement('div')
             div.id = `row-${i + 1}`
