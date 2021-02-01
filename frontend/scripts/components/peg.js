5e9503f1538df60d4c229fa7449c47f1ba259135
ajkelso <ajkelso@gmail.com>
2021-01-31 20:54:56 -0500

    adjust board size & some styles

--- frontend/scripts/components/peg.js
+++ frontend/scripts/components/peg.js
@@ -7,18 +7,18 @@ class Peg {
     addToCanvas = (canvas) => {
         this.html = document.createElement('div')
         let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
-        svg.setAttribute('width', '30')
-        svg.setAttribute('height', '30')
+        svg.setAttribute('width', '20')
+        svg.setAttribute('height', '20')
         let circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle')
         circle.classList.add('peg')
-        circle.setAttribute('cx', '15')
-        circle.setAttribute('cy', '15')
-        circle.setAttribute('r', '14.5')
-        circle.setAttribute('stroke', 'black')
+        circle.setAttribute('cx', '10')
+        circle.setAttribute('cy', '10')
+        circle.setAttribute('r', '9.5')
+        circle.setAttribute('stroke', 'grey')
         circle.setAttribute('stroke-width', '1')
         circle.setAttribute('fill', `${this.color}`)
     //change color event
-        circle.addEventListener('mousedown', () => {
+        svg.addEventListener('mousedown', () => {
             circle.setAttribute('fill', `${currentColor}`)
             if (resetButton.disabled && currentColor != null){
                 resetButton.disabled = false
@@ -28,7 +28,7 @@ class Peg {
             }
         })
 
-        circle.addEventListener('mouseover', () => {
+        svg.addEventListener('mouseover', () => {
             if (mouseIsDown){
                 circle.setAttribute('fill', `${currentColor}`)
             }
